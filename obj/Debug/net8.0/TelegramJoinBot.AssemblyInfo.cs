f786b973ef10d400619362c7aac0e482ed58c507
Nazarii Boichyshyn <boichyshyn@gmail.com>
2026-07-20 01:11:24 +0300

    Time stamp
    Time stamp

--- obj/Debug/net8.0/TelegramJoinBot.AssemblyInfo.cs
+++ obj/Debug/net8.0/TelegramJoinBot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TelegramJoinBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5f67102b54526771ac74252a19b08b08cd9d0f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TelegramJoinBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TelegramJoinBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
